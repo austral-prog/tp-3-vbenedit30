609591579f2e5756be2cb6e7a8c9c88214f12ce2
vbenedit30 <vbenedit@mail.austral.edu.ar>
2025-04-11 11:23:39 -0300

    Update in_string.py

--- in_string.py
+++ in_string.py
@@ -1,5 +1,6 @@
 def check_vowels():
-    print("Contiene a:", "a" in nombre or "A" in nombre)
+	nombre = input(("Ingresa tu nombre: ").lower())
+	print("Contiene a:", "a" in nombre or "A" in nombre)
 	print("Contiene e:", "e" in nombre or "E" in nombre)
 	print("Contiene i:", "i" in nombre or "I" in nombre)
 	print("Contiene o:", "o" in nombre or "O" in nombre)
