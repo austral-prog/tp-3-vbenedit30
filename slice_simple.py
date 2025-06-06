8d3815e3430ec716ac158d290e34f1a0a8bd08ad
vbenedit30 <vbenedit@mail.austral.edu.ar>
2025-06-06 11:55:06 -0300

    Update slice_simple.py

--- slice_simple.py
+++ slice_simple.py
@@ -1,5 +1,6 @@
 def slice_simple():
     texto = "Awesome"
-    print (texto[0:3])
-    print (texto[2:5])
-    print (texto[0:4]+texto[4:7])
+
+    print(texto[:3].lower())
+    print(texto[2:5])
+    print(texto.lower())
